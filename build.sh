151f5dd3c7f641855f8fd9e83085393481d4045e
Pander <pander@users.sourceforge.net>
2017-04-21 11:35:53 +0200

    Version 2.0

--- build.sh
+++ build.sh
@@ -1,32 +1,32 @@
-cp -f src/fonts/Neude_beta.?tf .
+cp -f src/fonts/Neude.?tf .
 
-ttx -o specimens/Neude_beta-otf.ttx Neude_beta.otf
-ttx -o specimens/Neude_beta-ttf.ttx Neude_beta.ttf
+ttx -o specimens/Neude-otf.ttx Neude.otf
+ttx -o specimens/Neude-ttf.ttx Neude.ttf
 
 #OTF_MODIFIED <modified value="
 #TTF_MODIFIED <modified value="
 
-OTF_VERSION=`grep '      <version value="' specimens/Neude_beta-otf.ttx|awk -F '"' '{print $2}'`
-#TTF_VERSION=`grep '      <version value="' specimens/Neude_beta-ttf.ttx|awk -F '"' '{print $2}'`
+OTF_VERSION=`grep '      <version value="' specimens/Neude-otf.ttx|awk -F '"' '{print $2}'`
+#TTF_VERSION=`grep '      <version value="' specimens/Neude-ttf.ttx|awk -F '"' '{print $2}'`
 TTF_VERSION=$OTF_VERSION
 
-OTF_GLYPHS=`grep '<GlyphID id="' specimens/Neude_beta-otf.ttx|grep -v '.null'|grep -v '.notdef'|grep -v 'nonmarkingreturn'|wc -l`
-TTF_GLYPHS=`grep '<GlyphID id="' specimens/Neude_beta-ttf.ttx|grep -v '.null'|grep -v '.notdef'|grep -v 'nonmarkingreturn'|wc -l`
+OTF_GLYPHS=`grep '<GlyphID id="' specimens/Neude-otf.ttx|grep -v '.null'|grep -v '.notdef'|grep -v 'nonmarkingreturn'|wc -l`
+TTF_GLYPHS=`grep '<GlyphID id="' specimens/Neude-ttf.ttx|grep -v '.null'|grep -v '.notdef'|grep -v 'nonmarkingreturn'|wc -l`
 
-fntsample -f Neude_beta.otf -o specimens/Neude_beta-otf-fntsample.pdf
-fntsample -f Neude_beta.ttf -o specimens/Neude_beta-ttf-fntsample.pdf
+fntsample -f Neude.otf -o specimens/Neude-otf-fntsample.pdf
+fntsample -f Neude.ttf -o specimens/Neude-ttf-fntsample.pdf
 
-fntsample -g -i 0x0000-0x007F -f Neude_beta.otf -o specimens/Neude_beta-otf-fntsample-basic-latin.svg
-fntsample -g -i 0x0000-0x007F -f Neude_beta.ttf -o specimens/Neude_beta-ttf-fntsample-basic-latin.svg
+fntsample -g -i 0x0000-0x007F -f Neude.otf -o specimens/Neude-otf-fntsample-basic-latin.svg
+fntsample -g -i 0x0000-0x007F -f Neude.ttf -o specimens/Neude-ttf-fntsample-basic-latin.svg
 
-inkscape -z -e specimens/Neude_beta-otf-fntsample-basic-latin.png specimens/Neude_beta-otf-fntsample-basic-latin.svg
-inkscape -z -e specimens/Neude_beta-ttf-fntsample-basic-latin.png specimens/Neude_beta-ttf-fntsample-basic-latin.svg
+inkscape -z -e specimens/Neude-otf-fntsample-basic-latin.png specimens/Neude-otf-fntsample-basic-latin.svg
+inkscape -z -e specimens/Neude-ttf-fntsample-basic-latin.png specimens/Neude-ttf-fntsample-basic-latin.svg
 
-convert -flatten specimens/Neude_beta-otf-fntsample-basic-latin.png specimens/Neude_beta-otf-fntsample-basic-latin.png
-convert -flatten specimens/Neude_beta-ttf-fntsample-basic-latin.png specimens/Neude_beta-ttf-fntsample-basic-latin.png
+convert -flatten specimens/Neude-otf-fntsample-basic-latin.png specimens/Neude-otf-fntsample-basic-latin.png
+convert -flatten specimens/Neude-ttf-fntsample-basic-latin.png specimens/Neude-ttf-fntsample-basic-latin.png
 
-fontimage --pixelsize 64 --text NEUDE_BETA --pixelsize 42 --text 0123456789\ \"\'+=^\`\| --text ABCDEFGHIJKLMNOPQRSTUVWXYĲZ --pixelsize 32 --text BLOWZY\ NIGHT-FRUMPS\ VEX\'D\ JACK\ Q. --text LYNX\ C.Q.\ VOS\ PRIKT\ BH\:\ DAG\ ZWEMJUF\! --text $OTF_GLYPHS\ GLYPHS\ V.\ $OTF_VERSION -o specimens/Neude_beta-otf-fontimage.png Neude_beta.otf
-fontimage --pixelsize 64 --text NEUDE_BETA --pixelsize 42 --text 0123456789\ \"\'+=^\`\| --text ABCDEFGHIJKLMNOPQRSTUVWXYĲZ --pixelsize 32 --text BLOWZY\ NIGHT-FRUMPS\ VEX\'D\ JACK\ Q. --text LYNX\ C.Q.\ VOS\ PRIKT\ BH\:\ DAG\ ZWEMJUF\! --text $TTF_GLYPHS\ GLYPHS\ V.\ $TTF_VERSION -o specimens/Neude_beta-ttf-fontimage.png Neude_beta.ttf
+fontimage --pixelsize 64 --text NEUDE --pixelsize 42 --text 0123456789\ \"\'+=^\`\| --text ABCDEFGHIJKLMNOPQRSTUVWXYĲZ --pixelsize 32 --text BLOWZY\ NIGHT-FRUMPS\ VEX\'D\ JACK\ Q. --text LYNX\ C.Q.\ VOS\ PRIKT\ BH\:\ DAG\ ZWEMJUF\! --text $OTF_GLYPHS\ GLYPHS\ V.\ $OTF_VERSION -o specimens/Neude-otf-fontimage.png Neude.otf
+fontimage --pixelsize 64 --text NEUDE --pixelsize 42 --text 0123456789\ \"\'+=^\`\| --text ABCDEFGHIJKLMNOPQRSTUVWXYĲZ --pixelsize 32 --text BLOWZY\ NIGHT-FRUMPS\ VEX\'D\ JACK\ Q. --text LYNX\ C.Q.\ VOS\ PRIKT\ BH\:\ DAG\ ZWEMJUF\! --text $TTF_GLYPHS\ GLYPHS\ V.\ $TTF_VERSION -o specimens/Neude-ttf-fontimage.png Neude.ttf
 
 echo
 echo 'TTF version: '$TTF_VERSION
